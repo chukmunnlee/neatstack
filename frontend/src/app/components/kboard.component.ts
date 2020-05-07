0a5e99dc8192e122c61e7246e9bf18a848735305
Chuk <chukmunnlee@gmail.com>
2020-05-07 15:27:24 +0800

    added kboard component

---
+++ frontend/src/app/components/kboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kboard',
+  templateUrl: './kboard.component.html',
+  styleUrls: ['./kboard.component.css']
+})
+export class KboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
